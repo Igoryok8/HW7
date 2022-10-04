56b18a920bd4c1d618890a483309a35a9dac1d92
Igoryok8 <energymag@mail.ru>
2022-10-05 00:03:45 +0300

    ДЗ №2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,18 @@ for (int i =1; i <=10; i++) {
         for (int i =10; i >=-10; i--) {
             System.out.println(+i);
         }
+        System.out.println("Домашнее задание 2");
+        System.out.println("Задание 1");
+        for (int i =1904; i <=2096; i=i+4) {
+            System.out.println("Высокосный год " + i);
+        }
+        System.out.println("Задание 2");
+        for (int i =7; i <=98; i=i+7) {
+            System.out.println( + i);
+        }
+        System.out.println("Задание 3");
+        for (int i =1; i <=512; i=i*2) {
+            System.out.println( + i);
+        }
     }
 }

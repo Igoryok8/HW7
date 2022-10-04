112859fa9e5f8e848724f692d37c3e80047c6d1d
Igoryok8 <energymag@mail.ru>
2022-10-04 23:48:01 +0300

    ДЗ №1 выполнено

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ public class Main {
 
         System.out.println("Домашнее задание 1");
         System.out.println("Задание 1");
-
+for (int i =1; i <=10; i++) {
+    System.out.println( + i);
+}
+        System.out.println("Задание 2");
+        for (int i =10; i >=1; i--) {
+            System.out.println(+i);
+        }
+        System.out.println("Задание 3");
+        for (int i =0; i <=17; i=i+2) {
+            System.out.println(+i);
+        }
+        System.out.println("Задание 4");
+        for (int i =10; i >=-10; i--) {
+            System.out.println(+i);
+        }
     }
 }
